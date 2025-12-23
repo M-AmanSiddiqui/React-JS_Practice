9baf13ac88d03c5ea72f06ed7ea9789ba650493f
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-12-23 16:06:07 +0500

    darkmode lightmode with useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import UserContextProvider from './context/UserContextProvider'
-import Login from './components/login'
-import Profile from './components/profile'
+
 
 function App() {
   return (
-    <UserContextProvider>
-      {/* Conditional rendering */}
-      <Login />  {/* Login component show karega agar user null hai */}
-      <Profile /> {/* Profile show karega agar user exist kare */}
-    </UserContextProvider>
+  
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+
   )
 }
 
