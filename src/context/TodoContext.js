53ed4a02947b66f42c4e4d23d098b083e200b856
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-12-25 19:06:34 +0500

    working

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { createContext,useContext  } from 'react'
+import { createContext, useContext } from "react";
 
-const ThemeContext = createContext({
-  themeMode: "light",
-  darkTheme: () => {},
-  lightTheme: () => {},
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo msg",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id,todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {}
 })
 
-export const ThemeProvider = ThemeContext.Provider
 
-export default function useTheme() {
- return useContext(ThemeContext) 
+export const useTodo = () => {
+  return useContext(TodoContext)
 }
+export const Todoprovider = TodoContext.Provider
